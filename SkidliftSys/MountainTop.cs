e4ff521d742cd44b7e26aa0a2bc09613dfde3be4
Gustav Lannhard <0325lagu@jonkoping.se>
2021-11-08 18:13:33 +0100

    Added MountainTop

---
+++ SkidliftSys/MountainTop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkidliftSys
+{
+    class MountainTop : Location
+    {
+        //Many Lifts may lead to this place, and people may spend some time waiting here.
+
+        public void TopOfMountainMove(int timestep)
+        {
+            foreach(Person i in occupants)
+            {
+                i.DecisionHandler(possiblemovements, this).MovePerson(i, this);
+                //Class may seem unnessecary but it will govern what type of choices Persons make
+            }
+        }
+
+
+    }
+}
